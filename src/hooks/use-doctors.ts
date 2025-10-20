22be18349ea3a6dddad6da8ba38a8db746165df1
manojLovesCoding <mkum9417@gmail.com>
2025-10-20 11:49:06 +0530

    feat(admin): implement full doctor management with stats and auto revalidation
    Added Admin Dashboard enhancements with live doctor and appointment statistics. Integrated AdminStats and DoctorsManagement components for better data visibility. Introduced Prisma server actions (getDoctors, createDoctor, updateDoctor). Added form validation and unique email constraint handling. Implemented automatic revalidation using revalidatePath('/admin') after data mutations. Improved loading state with a responsive LoadingUI component. Enhanced overall admin workflow for managing doctors and appointments efficiently

--- src/hooks/use-doctors.ts
+++ src/hooks/use-doctors.ts
@@ -1,7 +1,7 @@
 "use client";
 
-import { getDoctors } from "@/lib/actions/doctors";
-import { useQuery } from "@tanstack/react-query";
+import { createDoctor, getDoctors, updateDoctor } from "@/lib/actions/doctors";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 export function useGetDoctors() {
   const result = useQuery({
@@ -10,4 +10,31 @@ export function useGetDoctors() {
   });
 
   return result;
+}
+
+export function useCreateDoctor() {
+  const queryClient = useQueryClient();
+
+  const result = useMutation({
+    mutationFn: createDoctor,
+    onSuccess: () => {
+      // invalidate related queries to refresh the data
+      queryClient.invalidateQueries({ queryKey: ["getDoctors"] });
+    },
+    onError: (error) => console.log("Error while  creating a doctor"),
+  });
+
+  return result;
+}
+
+export function useUpdateDoctor() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: updateDoctor,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["getDoctors"] });
+    },
+    onError: (error) => console.error("Failed to update doctor:", error),
+  });
 }
